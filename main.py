8fe4562ba2267292bce567c95978f939e7bf01cf
chuka <emry390@gmail.com>
2023-09-26 19:38:07 +0100

    Day 24: Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from fpdf import FPDF
+import pandas as pd
+
+df = pd.read_csv('topics.csv')
+
+pdf = FPDF(orientation='P', unit='mm', format='A4')
+for index, rows in df.iterrows():
+    pdf.add_page()
+
+    pdf.set_font(family='Times', style='B', size=24)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=24, txt=rows["Topic"], ln=1, align='L')
+    # the numbers below represents: x1,y1 x2,y2(i.e. coordinates)
+    pdf.line(10, 26, 200, 26)
+
+pdf.output('output.pdf')
